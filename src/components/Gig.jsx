1042c76a1d8be7aa61ecddf4b4348b469c1f1ccd
dan gullis <daniel.gullis1@gmail.com>
2024-01-23 16:45:13 +0000

    implement sharing state for Gig component

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -2,13 +2,35 @@ import React from 'react'
 import "../Gig.css"
 import { useState } from "react";
 
+const FavouriteGig = (props) => {
+    const handleFavouriteToggle = () => {
+        console.log("clicked on button")
+        props.setFavourite(!props.isFavourite)
+    }
+    return <div>
+        {props.isFavourite ? (
+            <div>
+                <p className="favourited">In favourites</p>
+                <button data-testid="favourite" onClick={handleFavouriteToggle}>
+                    unfavourite
+                </button>
+            </div>
+        ) : (
+            <div>
+                <p className="favourited">Not in favourites</p>
+                <button data-testid="favourite" onClick={handleFavouriteToggle}>
+                    favourite
+                </button>
+            </div>
+        )}
+    </div>
+}
+
 const Gig = (props) => {
 
     const [isFavourite, setFavourite] = useState(false);
 
-    const handleFavouriteToggle = () => {
-        setFavourite(!isFavourite)
-    }
+    
     return (
         <div className="gig-listing">
             <img className="artist-image" src={props.imageSrc} alt={props.name}></img>
@@ -16,24 +38,7 @@ const Gig = (props) => {
             <p className="event-description"> {props.eventDescription} </p>
             <p className="time-date"> {props.timeDate}</p>
             <p className="location"> {props.location} </p>
-            <div>
-                {isFavourite ? (
-                    <div>
-                        <p className="favourited">In favourites</p>
-                        <button data-testid="favourite" onClick={handleFavouriteToggle}>
-                            unfavourite
-                        </button>
-                    </div>
-                ) : (
-                    <div>
-                        <p className="favourited">Not in favourites</p>
-                        <button data-testid="favourite" onClick={handleFavouriteToggle}>
-                            favourite
-                        </button>
-                    </div>
-                )}
-            </div>
-            
+            <FavouriteGig setFavourite={setFavourite} isFavourite={isFavourite}/>
         </div>
     );
 };
